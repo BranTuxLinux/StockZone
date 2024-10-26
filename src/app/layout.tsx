24d71ac0dbc64f82b149a12b2ac1d429480862c9
Brandon Quintero <brandonquinterogit@gmail.com>
2024-10-26 16:47:15 -0500

    Add auth0 and init config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
-
+import { UserProvider } from "@auth0/nextjs-auth0/client";
+import { Navbar } from "@/components/Navbar";
 
 export const metadata: Metadata = {
   title: "STOCKZONE",
@@ -14,11 +15,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className="h-full"
-      >
-        {children}
-      </body>
+      <UserProvider>
+        <body className="w-full flex flex-col items-center">
+          <Navbar  />
+          {children}
+
+        </body>
+      </UserProvider>
     </html>
   );
 }
