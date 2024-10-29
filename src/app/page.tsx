c6821aa5cc5b8586a0ad4e5f8b6c3b6b9a261643
Brandon Quintero <brandonquinterogit@gmail.com>
2024-10-28 19:33:50 -0500

    Add nav and design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
-"use client"
-import { useUser } from "@auth0/nextjs-auth0/client";
+"use client";
+import {  useUser } from "@auth0/nextjs-auth0/client";
 import Link from "next/link";
 
-
 export default function Home() {
-  const {user, isLoading,} =useUser()
+  const { user, isLoading } = useUser();
+  
+
   return (
     <>
       <h1>Home</h1>
-      {isLoading ? 'Cargando...': user?.name}
-      {!user ? 'No ha iniciado session': ""}
       <hr />
       <Link href={"/dashboard"}>Ir a dashboard </Link>
       <hr />
@@ -23,3 +22,4 @@ export default function Home() {
     </>
   );
 }
+
